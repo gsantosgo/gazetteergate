72c52289a94d94459d546913340c4b4557a422ea
Guillermo Santos <gsantosgo@yahoo.es>
2012-04-26 13:26:33 +0200

    version JRCNames

---
+++ gazetter-gate/src/main/java/es/uem/gazetteer/geonames/main/Main.java
@@ -0,0 +1,91 @@
+package es.uem.gazetteer.geonames.main;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Stopwatch;
+import com.google.common.io.Files;
+
+import es.uem.gazetteer.jrc.lineprocessor.JRCNamesLineProcessor;
+
+/**
+ * Main class (Geonames)
+ * 
+ * @author Guillermo Santos (gsantosgo@yahoo.es) 
+ */
+public class Main {
+	
+	final static String EXTENSION = ".lst";  
+	
+	@SuppressWarnings("serial")
+	final static Map<String, String> filters = new HashMap<String, String>() { 
+	{		
+	    put("A", "Country_State_Region");
+	    put("H", "Stream_Lake");
+	    put("L", "Parks_Area");
+	    put("P", "City_Village");
+	    put("R", "Road_RailRoad");
+	    put("S", "Spot_Building_Farm");
+	    put("T", "Mountain_Hill_Rock");
+	    put("U", "Undersea");
+	    put("V", "Forest_Heath");	    	    
+	}};	
+	
+	/**
+	 * 
+	 * Main program to generate Person, Organization files for GATE 
+	 * using JRCNames Entities sources (http://optima.jrc.it/data/entities.gzip) 
+	 *      
+	 * @param args
+	 */
+	public static void main(String[] args) {						
+		String inputFileNamePath = "src/main/resources/entities";
+		String outputDirectoryNamePath = "src/main/resources";
+		
+		
+		Preconditions.checkNotNull(inputFileNamePath, "Input filename should NOT be NULL");
+		Preconditions.checkNotNull(outputDirectoryNamePath, "Output directory should NOT be NULL.");
+		
+		File inputFile = new File(inputFileNamePath); 
+		File outputDirectory = new File(outputDirectoryNamePath);
+			
+		Preconditions.checkArgument(inputFile.exists(), "File does not exist: %s", inputFile);		
+		Preconditions.checkArgument(outputDirectory.exists(), "Directory does not exist: %s", outputDirectory);		 
+		
+		 Set<String> filterSet = filters.keySet();
+		 Iterator<String> iterator = filterSet.iterator();
+		 String filter = ""; 
+		 while (iterator.hasNext()) {
+			 filter = iterator.next();					
+			System.out.println(String.format("Starting process for filter %s ....", filter));
+			System.out.println("> Waiting....");
+			Stopwatch stopWatch = new Stopwatch();
+			stopWatch.start();
+			String resultado = "";
+			try {
+				File outputFile = new File(outputDirectoryNamePath + File.separator + filters.get(filter) + EXTENSION);
+				// if file exists then remove it				
+				if (outputFile.exists()) outputFile.delete();				
+				resultado = Files.readLines(inputFile, Charsets.UTF_8,
+						new JRCNamesLineProcessor(filter, outputFile));
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
+			System.out.println(String.format(
+					"For filter %s, processed registers %s ", filter, resultado));	
+			System.out.println(String.format(
+					"Complete process. Elapsed time %d min, %d sec.",
+					stopWatch.elapsedTime(TimeUnit.MINUTES),
+					stopWatch.elapsedTime(TimeUnit.SECONDS)));
+			System.out.println("");		
+		} 
+	}	
+			
+}
---
+++ gazetter-gate/src/main/java/es/uem/gazetteer/jrc/lineprocessor/JRCNamesLineProcessor.java
@@ -0,0 +1,72 @@
+package es.uem.gazetteer.jrc.lineprocessor;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Strings;
+import com.google.common.io.Files;
+import com.google.common.io.LineProcessor;
+
+/**
+ * JRCNames LineProcessor 
+ * 
+ * @author Guillermo Santos (gsantosgo@yahoo.es) 
+ */
+public class JRCNamesLineProcessor implements LineProcessor<String> {
+	public final String SEPARATOR = "\t";
+	public final char MULTI_WORD_SPACE = '+';
+	private int lineCount = 0;
+	private String filter = null;
+	private File file = null;
+
+	/** 
+	 * Constructor 
+	 * 
+	 * @param filter Search filter  
+	 * @param file File 
+	 */
+	public JRCNamesLineProcessor(String filter, File file) {
+		this.filter = filter;
+		this.file = file;
+	}
+	
+	/*
+	 * =============================================== 
+	 * Informaci�n estructura del fichero JRCNames 
+	 * Formato UTF-8 
+	 * Columnas id;type;language;name Each line
+	 * 
+	 * consists of four tab-separated columns containing: name ID; type;
+	 * language; and name variant (see Table 2). Name ID is a unique numerical
+	 * identifier for the entity. In this release, Type can only be Person (P)
+	 * or Organisation (O). The column Language contains the ISO 639-2 two-digit
+	 * code for the language if the name variant should only be looked up in
+	 * that language. If a name can be looked up in all languages, which is the
+	 * default, the value is u (undefined). The strings in the column name
+	 * variant are the known spellings of the name, one per line. Multi-word
+	 * strings are separated by the + sign (e.g. United+Nations).
+	 */
+
+	public boolean processLine(String line) throws IOException {
+		if (!Strings.isNullOrEmpty(line)) {
+			String[] columns = line.split(SEPARATOR);
+			if (columns.length == 4) {
+				String organization = columns[1];
+				String name = columns[3];
+				if (organization.equals(this.filter)
+						&& !Strings.isNullOrEmpty(name)) {
+					lineCount++;
+					Files.append(name.replace(MULTI_WORD_SPACE, ' ') + "\n", this.file,
+							Charsets.UTF_8);
+				}
+			}
+		}
+		return true;
+	}
+
+	public String getResult() {
+		return new StringBuffer().append(lineCount).toString();
+	}
+
+}
